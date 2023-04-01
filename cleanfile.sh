1ade0b546ecc80fe2f1bf2b68817e2f1bbe1570e
Will Kibbe <14157203-wkibbe@users.noreply.gitlab.com>
2023-04-01 20:26:41 +0000

    Update file cleanfile.sh

--- cleanfile.sh
+++ cleanfile.sh
@@ -1,4 +1,4 @@
-#author of this shell program: Michele Samorani
+#author of this shell program: unknown
 shopt -s extglob # Enable Bash Extended Globbing expressions
 IFS=
 while read -r line || [[ "$line" ]]; do
